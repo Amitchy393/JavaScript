801c3e21615c58706db4eb1bfee4f9f6d55582d0
Amit Kumar Chaudhary <77068907+Amitchy393@users.noreply.github.com>
2023-09-14 14:11:16 +0000

    container config done

---
+++ Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountNumbar =156789;
+let accountEmail = "amit@gmail.com";
+var accountPassword ='12345';
+accountCity = "Mumbai";
+
+let accountStatement;
+
+
+//accountNumber = 123; Not Allowed
+
+accountEmail = "kumar@gmail.com";
+accountPassword = "000";
+accountCity = "Delhi";
+
+console.table([accountNumbar, accountEmail, accountPassword, accountCity, accountStatement]);
+
+/*
+this is multi line comment 
+*/
