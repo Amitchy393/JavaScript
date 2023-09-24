f51f151d270d640b9235cb7a6fe666d547c18b58
Amit Kumar Chaudhary <77068907+Amitchy393@users.noreply.github.com>
2023-09-24 14:41:56 +0000

    DataTypes

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+
+//DATA TYPES:---->>>>
+
+// 1. Number
+// 2. String
+// 3. BigInt
+// 4. Boolean (true)
+// 5. Undefined
+// 6. Null   (Standlone value)
+// 7. Symbol  (unique)
+// 8. Onject
+
+let age=24;
+let name="Amit";
+let IsLogedIn="false";
+
